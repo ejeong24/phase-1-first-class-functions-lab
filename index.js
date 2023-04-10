716c9b0b2a2fa4603e19183b8b5d2b9897fd36c4
Eugene Tae Joo Jeong <ejeong24@gmail.com>
2023-04-10 01:56:32 -0400

    passed testing

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {return drivers.slice(0,2)};
+
+const returnLastTwoDrivers = function(drivers) {return drivers.slice(drivers.length-2,drivers.length)};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplyBy) {
+    return function (fare) {
+        return fare*multiplyBy;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+/*function fareDoubler(fare){
+    return createFareMultiplier(2)(fare);
+}*/
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers,firstOrLast) {
+
+return firstOrLast(drivers);
+
+
+}
+
+/*= function(fare) {
+    const doubled = createFareMultiplier(2);
+}
+
+
+/*function fareDoubler(fare){
+    return fare*2;  
+}
+
+function fareTripler(fare){
+    return fare*3;
+}*/
+
